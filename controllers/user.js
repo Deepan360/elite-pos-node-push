bc52b74171474d86bf1c1204be4c5a3f31c6edd0
Deepan360 <deepandeep686@gmail.com>
2024-07-24 10:03:42 +0530

    qre

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const sql = require('mssql');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
+
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
